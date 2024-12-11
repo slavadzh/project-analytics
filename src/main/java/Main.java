a3ac8cb13492e506038990f1d3265ba6b56f9b3e
slavadzh <vyacheslavdzhabarov@gmail.com>
2024-12-11 20:00:05 +0500

    Visualization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import analytics.models.Student;
 import analytics.util.Parser;
+import drawer.BarChartDrawer;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -7,17 +8,18 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        var students = Parser.readCSVFile("java-rtf.csv");
-
-        try {
-            OutputStream out = System.out;
-            PrintStream ps = new PrintStream(out, true, StandardCharsets.UTF_8);
-            System.setOut(ps);
-            for (var student : students) {
-                System.out.println(student);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+//        var students = Parser.readCSVFile("java-rtf.csv");
+//
+//        try {
+//            OutputStream out = System.out;
+//            PrintStream ps = new PrintStream(out, true, StandardCharsets.UTF_8);
+//            System.setOut(ps);
+//            for (var student : students) {
+//                System.out.println(student);
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+        /*new BarChartDrawer("Histogram")*/
     }
 }
--- src/main/java/analytics/controller/GroupController.java
+++ src/main/java/analytics/controller/GroupController.java
@@ -2,17 +2,26 @@ package analytics.controller;
 
 import analytics.entity.GroupEntity;
 import analytics.entity.StudentEntity;
+import analytics.mapper.ChartDataMapper;
 import analytics.models.Group;
 import analytics.models.Student;
 import analytics.service.GroupService;
 import analytics.util.Parser;
+import drawer.BarChartDrawer;
+import org.jfree.chart.JFreeChart;
 import org.modelmapper.ModelMapper;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -28,12 +37,41 @@ public class GroupController {
     }
 
     @GetMapping
-    public List<Group> getStudents() {
+    public List<Group> getGroups() {
         return groupService.findAll().stream().map(this::convertToGroup).toList();
+    }
+
+    @GetMapping("/chart")
+    public ResponseEntity<Resource> getBarChart() {
+        try {
+            BarChartDrawer drawer = new BarChartDrawer("Scores by Group");
+            File chartFile = drawer.createChartFile(getGroups(), "chart.png");
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"chart.png\"")
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(new FileSystemResource(chartFile));
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/chart/{id}")
+    public ResponseEntity<Resource> getBarChartForChapter(@PathVariable("id") String id) {
+        try {
+            BarChartDrawer drawer = new BarChartDrawer("Scores by Group");
+            File chartFile = drawer.createChartFile(getGroups(), id,"chart.png");
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"chart.png\"")
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(new FileSystemResource(chartFile));
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     public Group convertToGroup(GroupEntity groupEntity) {
         Group group = modelMapper.map(groupEntity, Group.class);
+        group.setName(groupEntity.getName().replace("Программирование на Java ЛБ, ", ""));
         group.getStudents().forEach(student -> student.setGroup(group.getName()));
         return group;
     }
